484c84f520296d9648092dcf5cb72131326e0a8c
Kenneth Arnesen <kenneth.metastate@yahoo.com>
2021-01-24 01:05:14 +0200

    API configured

---
+++ src/MetaState.IdentityServer4.Admin.Api/Configuration/Constants/AuthorizationConsts.cs
@@ -0,0 +1,7 @@
+﻿namespace MetaState.IdentityServer4.Admin.Api.Configuration.Constants
+{
+    public class AuthorizationConsts
+    {
+        public const string AdministrationPolicy = "RequireAdministratorRole";
+    }
+}
---
+++ src/MetaState.IdentityServer4.Admin.Api/Resources/IApiErrorResources.cs
@@ -0,0 +1,9 @@
+﻿using MetaState.IdentityServer4.Admin.Api.ExceptionHandling;
+
+namespace MetaState.IdentityServer4.Admin.Api.Resources
+{
+    public interface IApiErrorResources
+    {
+        ApiError CannotSetId();
+    }
+}
